bd61337475736c6c38386ed90ddcf2775e54a161
stephanie <stephaniechang2017@u.northwestern.edu>
2017-05-18 10:08:58 -0500

    omni linear control via velocities

--- src/omni_reference_targets.py
+++ src/omni_reference_targets.py
@@ -30,19 +30,22 @@ def calc_quat(th):
 ZERO_POS = np.array([0.749870470328, -0.466371185967,  0.0240209384569])
 #ZERO_POS = np.array([0.902160877388, -0.0715308389238, 0.284243124085]) #Sawyer start pos
 ZERO_ORI = calc_quat(0.0)
+
 XYZ_MIN = np.array([0.4, -0.4, 0.01]) #Sawyer Limits
 XYZ_MAX = np.array([0.82, 0.4, 0.25]) #Sawyer Limits
-XYZ_SCALE = np.array([0.1, 0.25, 0.25])
-XYZ_INVERT_MAP = np.array([-1, -1, 1])
-XYZ_INDEX_ARR = np.array([1,0,3])
+XYZ_SCALE = np.array([1, 1.25, 1.25])
+XYZ_INVERT_MAP = np.array([1, 1, 1])
+XYZ_INDEX_ARR = np.array([0,1,2])
+XYZ_OFFSET_ARR = np.array([0.206, 0.0, 0.047])
 TH_SCALE = 2.5
-TH_INDEX = 2
+TH_INDEX = 0 # Omni rotates about x-axis
 TH_MIN = -np.pi/2
 TH_MAX = np.pi/2
 FREQUENCY = 20
 REF_FRAME = "base"
 TARGET_FRAME = "target"
-STYLUS_FRAME = "stylus"
+OMNI_BASE_FRAME = "omni/base"
+STYLUS_FRAME = "omni/stylus"
 ON = True
 OFF = False
 
@@ -61,36 +64,58 @@ class SimpleTargets(object):
 
         # Instantiating keyboard object
         self.key = kbhit.KBHit()
+        rospy.on_shutdown(self.key.set_normal_term)
 
         #Subscribers and Publishers
         self.br = tf.TransformBroadcaster()
-        self.listen = tf.TransformListener()
-        self.key_timer = rospy.Timer(rospy.Duration(0.1), self.running_cb)
-
+        self.key_timer = rospy.Timer(rospy.Duration(0.01), self.running_cb)
         self.ref_pose_pub = rospy.Publisher('ref_pose', Pose, queue_size = 3)
         self.integrate_and_pub_timer = rospy.Timer(rospy.Duration(1/float(FREQUENCY)), self.timer_cb)
 
+        self.listen = tf.TransformListener()
         return
 
-#****************************
-# WIP
     def running_cb(self, key_msg):
         # Deadman switch
         if self.key.kbhit(): # Check if key pressed
             key_input = self.key.getch()
             if key_input == 's':
+                bad_key = False
                 desired_sys_state = ON
+                rospy.loginfo("Pressed 's'")
             elif key_input == 'f':
+                bad_key = False
                 desired_sys_state = OFF
+                rospy.loginfo("Pressed 'f'")
+            else:
+                bad_key = True
+                rospy.loginfo("Unknown key")
 
-            if self.run_state is not desired_sys_state:
-                self.run_state = desired_run_state
+            if (bad_key == False): # Checking for valid key
+                if self.run_flag is not desired_sys_state:
+                    self.run_flag = desired_sys_state
+                    rospy.loginfo("Running: %s", self.run_flag)
 
-        if (self.run_state): #Enable movement if 's' was pressed
-            # set velocity:
-            jvels = XYZ_INVERT_MAP*XYZ_SCALE*np.choose(XYZ_INDEX_ARR, joy_message.axes)
+#****************************
+#---WIP----------------------
+        # Enable movement if 's' pressed
+        if (self.run_flag):
+            try:
+                time = self.listen.getLatestCommonTime(OMNI_BASE_FRAME, STYLUS_FRAME)
+                omni_p, omni_quat = self.listen.lookupTransform(OMNI_BASE_FRAME, STYLUS_FRAME, time)
+            except(tf.Exception):
+                print "tf exception encountered"
+                return
+
+            p_list =  np.array(omni_p) - XYZ_OFFSET_ARR
+            quat_list = np.array(omni_quat)
+
+            # set velocity: Takes position as [px, py, pz] vector
+            jvels = XYZ_INVERT_MAP*XYZ_SCALE*np.choose(XYZ_INDEX_ARR, p_list)
+            # print jvels
+            # jvels = np.choose(XYZ_INDEX_ARR, p_list)
             self.ee_dot = np.array(jvels)
-            self.th_dot = TH_SCALE*joy_message.axes[TH_INDEX]
+            # self.th_dot = TH_SCALE*omni_quat[TH_INDEX]
         else:
             self.ee_dot = np.array([0,0,0])
             self.th_dot = 0.0
@@ -113,7 +138,6 @@ class SimpleTargets(object):
         self.ref_pose_pub.publish(Pose(Point(*self.position), Quaternion(*self.orientation)))
         self.br.sendTransform(self.position, self.orientation, rospy.Time.now(), TARGET_FRAME, REF_FRAME)
         return
-
 
 def main():
     rospy.init_node('omni_reference_pose_generator')
